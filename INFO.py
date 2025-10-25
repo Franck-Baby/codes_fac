16a3a605539303ae9b350c7fcabcdddcdee50402
Franck-Baby <franckndambala15@gmail.com>
2025-10-25 04:38:53 +0200

    Add files via upload

---
+++ INFO.py
@@ -0,0 +1,178 @@
+
+
+
+#Définition de notre fonction principale
+def Balayage():
+
+    
+    #Partie où l'utilisateur devra entrer les diverses valeurs qu'utilisera notre fonction principale  
+    print("Soit une fonction f définie sur un intervalle [a;b]")
+    a = float(input("Entrez la valeur de a : "))
+    b = float(input("Entrez la valeur de b : "))
+    e = float(input("Entrez la valeur seuil e : "))
+    
+    
+    x = a ;
+    y = b ;
+    S = x ;
+
+
+
+    #Définition de la fonction qui demandera à l'utilisateur d'entrer l'expression de sa fonction
+    def donner_fonct():
+        f = input("Veuillez entrer votre fonction, en fonction de x ")
+        return lambda x: eval(f)
+
+
+    
+     
+    f = donner_fonct() #Stackage de la fonction entrée par l'ulisateur dans la variable f 
+
+    print(" \nx = ", x,"\n y = ", y, "\n S = ", S)
+    
+    print("\n Les valeurs des images sont : \nf(",x,") = ", f(x))  #Affichage des valeurs de f(x) 
+    
+    if f(x)== 0 :
+        S = y
+        print(" x = ", x,"\n y = ", y, "\n S = ", S)
+
+    
+    while ((abs(y-x) >= e) and ( f(x)*f(y)< 0)): #La boucle "Tantque" avec les différentes conditions
+        
+        print("f(",x,") *  f(",y,") = ", f(x)*f(y)," et  |y-x| = ",abs(y-x) )
+        S = (x+y)/2
+        
+        if (f(x)*f(S) < 0):
+            y = S
+            
+        else:
+            x = S
+        print(" x = ", x,"\n y = ", y, "\n S = ", S)
+
+
+    print("\n\n On a en fin : ")
+    print("f(",x,") *  f(",y,") = ", f(x)*f(y)," et  |y-x| = ",abs(y-x) )
+    print(" x = ", x,"\n y = ", y, "\n S = ", S)
+        
+    return print("\n OUFF!! Terminé... \nLa valeur approchée de votre fonction à", e ,"près est :", S); # On retourne la valeur de S
+
+if __name__=="main" :
+    print(Balayage()) # Affichage du resultat final retouné par la fonction principale
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#On défini notre fonction principale 'Newton'
+def Newton():
+
+#Partie pour demander à l'utilisateur d'entrer les valeurs nécessaires     
+    u = float(input("Entrez la valeur initiale de u  :  "))
+    E = float(input("Entrez la valeur du seuil de précision :  "))
+    S = u;
+    print("\nInitialement on a :  u = ",u," et S = ", S)
+#On écris maintenant des fonctions pour demander à l'utilisateur d'entrer l'expression de sa fonction
+    def Entrer_fonction(): 
+        F = input("Entrez votre fonction en fonction de u (exemple: u**2 - 6u + 4):  ")
+        return lambda u: eval(F)
+
+# De même, on demande à l'ulisateur d'entrer l'expression de la dérrivée de sa fonction 
+    def Entrer_der():
+        Der_F = input("Entrez la dérrivée de votre fonction  :  ")
+        return lambda u: eval(Der_F)
+            
+#Ici on affecte les valeurs de la fonction puis celle de sa dérrivée correspondente respectivement à F et Der_F entrées par l'utilisateur
+    F = Entrer_fonction()
+    Der_F = Entrer_der()
+    
+            
+    if (Der_F(u) != 0 ) :
+        S = u - (F(u)/Der_F(u)) # calcul du terme suivant de U
+    
+    print("\n En suite S = ", S)
+    while ( (Der_F(S) != 0) and (abs(S-u) >= E ) ): # La structure tantque pour faire les test sur la dérrivée de f et la velur absolue de (s-u)
+        print("Der_F(",S,") = ", Der_F(S), " et |S-u| = ", abs(S-u) )
+
+        u = S
+        print("u = ", u)
+        print("Calcul de la nouvelle valeur de S")
+        S =u - (F(u)/Der_F(u))
+        print("la nouvelle valeur de S = ", S,"\n")
+        
+        
+    print(" \nOn a en fin   :  u = ", u, ";   S = ", S, "; avec Der_F(",S,") = ", Der_F(S), "  et  |S-u| = ", abs(S-u))
+    
+    return print( " \n\n\n            La valeur approchée de votre fonction à ",E," près est : S = ", S); # Retour du résultat final 
+
+if __name__ == "main" :
+    print(Newton())# Affichage final
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def Secante(): 
+
+    print("Soit votre fonction définie sur l'intervalle [a,b]");
+
+    a = float(input("Entrez la valeur de a :  ")) ;
+    b = float(input("Entrez la valeur de b :  ")) ; 
+    e = float(input("Entrez la valeur du seuil de précision :  ")) ;
+    S = a ;
+
+
+
+    def donne_fonc() :
+        W = input(" Veuillez entrer votre fonction  f(x) ");
+        return lambda x: eval(W)
+
+
+    f = donne_fonc() ;
+
+    if f(b) == 0 :
+        S = b ;
+
+    print("Les valeurs initialles sont :  a = ", a ," ;  b = ", b , " ; e = ", e , " ; S = ", S )
+    
+    while ( (f(a)*f(b) < 0 )  and (abs(b-a) >= e)  ) :
+        print("\nf(",a,") * f(",b,") = ", (f(a)*f(b)) ," et  |",a,"- ",b,"| = ", abs(b-a)) ;
+        
+        S = (a*f(b) - b*f(a))/(f(b) - f(a)) ;
+        
+        print("Donc le nouveau  S = ", S)
+        
+        if (f(S)*f(b)) < 0 :
+            a = S ;
+            print("Comme (f(S)*f(b)) = ",(f(S)*f(b)) ,"< 0  alors a = ", S)
+        else :
+            b = S ;
+            print("Comme (f(S)*f(b)) = ",(f(S)*f(b)) ," > 0  alors b = ", S)
+        
+    
+
+        
+    return print("\nf(",a,") * f(",b,") = ", (f(a)*f(b)) ," et  |",a,"- ",b,"| = ", abs(b-a),"\n alors on a finalement :  a = ",a," ; b = ",b," et  S = ",S,"\n\nLa solution approchée de votre fonction à ",e," près est  : ", S)
+        
+if __name__ == "__main__" :
+    print(Secante())
